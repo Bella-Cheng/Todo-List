939ac11c8c5b0716d87277fc8bc9199affde82dd
LAPTOP-7I4N09T2\Bella <dhp951320@gmail.com>
2025-05-10 00:26:35 +0800

    feat: use vie for add InputText push to todo list

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import {ref} from 'vue'
 
 //邏輯整理
@@ -12,12 +12,22 @@ import {ref} from 'vue'
 //按編輯：跳出視窗修改清單訊息 + 按完成：可以修改?
 //按刪除：可以將清單整個從list移除
 
-const 
+const todoInput = ref("")
+const todoList = ref([])
 
 
-function add(){
-  if(){
 
+
+
+function addButton(){
+  if(todoInput.value != ""){
+    todoList.value.push({
+    text:todoInput.value,
+    edit:false,
+    delete:false,
+    done:false
+  })
+    todoInput.value = ""
   }
 }
 
@@ -28,25 +38,16 @@ function add(){
     <div class="max-w-5xl m-auto bg-[#FFFFFF] p-[12px]">
       <h1 class="text-3xl font-bold text-center p-[50px] ">代辦清單 ToDo</h1>
       <div class="text-center">
-        <input class="w-[70%] p-[12px] border rounded-4xl mx-8 border-gray-700  focus:border-pink-600" type="text" placeholder="輸入事項"/>
-        <button type="button" class="btn btn-soft btn-primary p-[12px]">新增</button>
+        <input v-model="todoInput" class="w-[70%] p-[12px] border rounded-4xl mx-8 border-gray-700  focus:border-pink-600" type="text" placeholder="輸入事項"/>
+        <button @click="addButton" type="button" class="btn btn-soft btn-primary p-[12px]">新增</button>
       </div>
       <div class="border-b-1 mt-20 mb-8"></div>
-      <div class="flex justify-center items-center w-full mb-8">
+      <div v-for="(item, id) in todoList" :key="id" class="flex justify-center items-center w-full mb-8">
         <input type="checkbox" name="checkbox" id="" class="checkbox checkbox-primary"/>
-        <p class="p-[12px] w-[70%]">Todo清單</p>
+        <p class="p-[12px] w-[70%]">{{ item.text }}</p>
         <button class="after:content-['｜'] after:ml-2" type="button"><i class="fa-solid fa-pen p-[12px]"></i>修改</button>
         <button><i class="fa-solid fa-trash p-[12px]" type="button"></i>刪除</button>
       </div>
     </div>
  </div>
-
-  
-
-
-
 </template>
-
-<style scoped>
-
-</style>
