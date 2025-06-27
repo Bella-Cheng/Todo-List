5ba02225d94973fbbdb00b8d2b112a44510de3af
Bella-Cheng <dhp951320@gmail.com>
2025-06-28 00:36:56 +0800

    feat : add RWD

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
 import {ref} from 'vue'
-
-//邏輯整理
-//按下確認按鈕，確認使用者在input欄位沒有空值
-//按下按鈕完成，input欄位應該變回空值
-//新增的值應該push到下面的list清單
-
-//下方的list清單
-//按完成可以呈現黃底白字或用刪除線
-//按編輯：跳出視窗修改清單訊息 + 按完成：可以修改?
-//按刪除：可以將清單整個從list移除
 
 const todoInput = ref("")
 const todoList = ref([])
@@ -50,54 +39,70 @@ function updateEdit(){
 </script>
 
 <template>
-  <div class="bg-[#fcf1f7] w-screen h-screen ">
-    <div class="max-w-5xl m-auto bg-[#FFFFFF] p-[12px]">
-      <div class="flex w-full justify-center items-center mb-6">
-        <div class="w-[10%]">
-          <img class="w-full" src="./icon-book.png" alt="logo">
-        </div>
-        <div>
-          <h1 class="text-3xl font-bold text-center py-[50px] ">代辦清單 ToDo
-          </h1>
-        </div>
-      </div>
-
+  <div class="bg-[#fcf1f7] min-h-screen w-full px-4">
+    <div class="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow mt-8">
       
-      <div class="text-center w-full">
-        <input v-model="todoInput" class="w-[70%] p-[12px] border rounded-4xl mx-8 border-gray-700  focus:border-pink-600" type="text" placeholder="輸入文字"/>
-        <button @click="addButton" type="button" class="btn btn-soft btn-primary p-[12px]">新增</button>
+      <div class="flex items-center justify-center mb-6">
+        <img src="./icon-book.png" alt="logo" class="w-10 h-10 mr-4" />
+        <h1 class="text-3xl font-bold text-center">代辦清單 ToDo</h1>
       </div>
-      <div class="border-b-1 mt-20 mb-8"></div>
-      <div v-for="(item, id) in todoList" :key="id" class="flex justify-center items-center w-full py-4" 
-        :class="{ 'bg-gray-400 text-white': item.done }">
-        <input @change="item.done = !item.done" type="checkbox" name="checkbox" id="" class="checkbox checkbox-neutral"/>
-        <p class="p-[12px] w-[70%]" :class="{ 'line-through': item.done }">{{ item.text }}</p>
 
-        <button @click="openEditModal(item)" type="button" class="group relative cursor-pointer">
-          <i class="fa-solid fa-pen group-hover:text-indigo-500 mr-2 "></i>
-          <span class="group-hover:text-indigo-500">編輯
-          </span>
-          <span class="px-4 text-black">｜</span>
-        </button>
-        <dialog id="editPrompt" class="modal">
-          <div class="modal-box max-w-2xl m-auto">
-            <h3 class="text-lg font-bold">編輯文字</h3>
-            <input v-model="editingText" class="py-4 my-8 input w-full" type="text" />
-            <div class="modal-action">
-              <form method="dialog">
-              <!-- if there is a button, it will close the modal -->
-                <button @click="updateEdit" class="btn btn-soft btn-primary mr-4">更新</button>
-                <button class="btn btn-soft btn-neutral">關閉</button>
-              </form>
-            </div>
+      <div class="flex flex-col md:flex-row items-center justify-center gap-4 mb-6">
+        <input
+          v-model="todoInput"
+          type="text"
+          placeholder="輸入文字"
+          class="w-full md:w-2/3 p-3 border rounded focus:outline-none focus:border-pink-600"
+        />
+        <button @click="addButton" class="btn btn-primary px-6 py-3">新增</button>
+      </div>
+
+      <hr class="mb-6" />
+
+      <div
+        v-for="(item, id) in todoList"
+        :key="id"
+        class="border-b py-4 flex flex-col md:flex-row md:items-center md:justify-between gap-4"
+        :class="{ 'bg-gray-400 text-white': item.done }"
+      >
+
+        <div class="flex items-start md:items-center gap-4 w-full md:w-auto">
+          <input
+            type="checkbox"
+            v-model="item.done"
+            class="checkbox checkbox-neutral mt-1 md:mt-0"
+          />
+          <p
+            :class="{ 'line-through': item.done }"
+            class="text-base md:text-lg"
+          >
+            {{ item.text }}
+          </p>
+        </div>
+
+        <div class="flex gap-4 text-sm md:text-base items-center justify-end md:justify-normal">
+          <button @click="openEditModal(item)" class="hover:text-indigo-500 flex items-center gap-1">
+            <i class="fa-solid fa-pen"></i><span>編輯</span>
+          </button>
+          <button @click="deleteButton(item)" class="hover:text-pink-600 flex items-center gap-1">
+            <i class="fa-solid fa-trash"></i><span>刪除</span>
+          </button>
+        </div>
+      </div>
+
+      <dialog id="editPrompt" class="modal">
+        <div class="modal-box max-w-2xl m-auto">
+          <h3 class="text-lg font-bold">編輯文字</h3>
+          <input v-model="editingText" class="py-4 my-8 input w-full" type="text" />
+          <div class="modal-action">
+            <form method="dialog">
+              <button @click="updateEdit" class="btn btn-primary mr-4">更新</button>
+              <button class="btn btn-neutral">關閉</button>
+            </form>
           </div>
-        </dialog>
-
-        <button @click="deleteButton(item)" class="hover:text-pink-600 cursor-pointer flex items-center" type="button">
-          <i class="fa-solid fa-trash mr-2 " type="button"></i>
-          <span>刪除</span>
-        </button>
-      </div>
+        </div>
+      </dialog>
     </div>
- </div>
+  </div>
 </template>
+
